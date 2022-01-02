2127808df40b28c5ff91c33c7b9aa49388936aba
Jason Robinson <robinsonjason761@gmail.com>
2022-01-01 23:23:36 -0600

    adding arxivscraper directory and constants

--- notebooks/arxiv_demo.py
+++ notebooks/arxiv_demo.py
@@ -16,7 +16,8 @@ df = pd.DataFrame(output, columns=cols)
 scraper = ax.Scraper(category='stat',date_from='2017-08-01',date_until='2017-08-10',t=10, filters={'categories':['stat.ml'],'abstract':['learning']})
 output = scraper.scrape()
 
-
+cols = ['title','id','abstract','categories','doi','created','updated','authors','url']
+df = pd.DataFrame(output, columns=cols)
 
 
 "Citations: Mahdi Sadjadi (2017). arxivscraper:Zendo. http://doi.org/10.5281/zenodo.889853"
